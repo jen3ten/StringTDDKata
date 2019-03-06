d77aa2c4c8b099c2835b118e98a60444b17b7aac
Jen E <jen@wecancodeit.org>
2019-03-06 10:19:00 -0500

    Initial commit

---
+++ StringTDDKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace StringTDDKata.Tests
+{
+    public class StringCalculatorTests
+    {
+        private StringCalculator sut;
+
+
+        [Fact]
+        public void Add_Returns_0_For()
+        {
+
+        }
+    }
+}
---
+++ StringTDDKata/StringCalculator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StringTDDKata
+{
+    public class StringCalculator
+    {
+    }
+}
